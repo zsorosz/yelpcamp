00befa2137ffb012d9981518244bf1dc86060cf2
Zsofia <oroszi.zsofi@gmail.com>
2019-09-02 17:10:20 +0200

    Set up Initial Routes

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.send("this will be the landing page soon!");
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [
+        {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"},
+        {name: "Granite Hill", image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"},
+        {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"},
+    ]
+
+    res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 var port = process.env.PORT || 3000;
