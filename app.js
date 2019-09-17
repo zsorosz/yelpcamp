6711722500bd02765be0e3d76d0e0bacaae4f08e
Zsofia <oroszi.zsofi@gmail.com>
2019-09-17 15:17:51 +0200

    Set up Passport for user authentication

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    Campground  = require("./models/campground"),
-    seedDB      = require("./seeds")
-    Comment     = require("./models/comment")
-    // User        = require("./models/user")
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    Campground      = require("./models/campground"),
+    seedDB          = require("./seeds")
+    Comment         = require("./models/comment")
+    User            = require("./models/user")
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+// PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "Once again Cseppke wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Campground.create(
 //     {
