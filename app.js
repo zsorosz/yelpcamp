b7814fbda3fb2d1a050c877e948e8f2b3028b570
Zsofia <oroszi.zsofi@gmail.com>
2019-09-17 18:23:55 +0200

    Hide links in navbar

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
 // Campground.create(
 //     {
 //         name: "Granite Hill",
@@ -59,7 +64,7 @@ app.get("/campgrounds", function(req, res){
         if(err){
             console.log(err);
         } else{
-            res.render("campgrounds/index", {campgrounds: allCampgrounds});
+            res.render("campgrounds/index", {campgrounds: allCampgrounds, currentUser: req.user});
         }
     });
 });
