79c649dda117039d068334cb800a8e5adc398260
Zsofia <oroszi.zsofi@gmail.com>
2019-09-17 15:36:41 +0200

    Add signup form and routes

--- app.js
+++ app.js
@@ -129,6 +129,28 @@ app.post("/campgrounds/:id/comments", function(req, res){
     })
 });
 
+//================
+// AUTH ROUTES
+//================
+
+// show register form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+// handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("The YelpCamp Server Has Started!");
