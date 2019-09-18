55dab2f5f1efb55e0a8a679b51510d44ac42aa6a
Zsofia <oroszi.zsofi@gmail.com>
2019-09-18 14:56:18 +0200

    Associate campgrounds and users

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,12 +15,16 @@ router.get("/", function(req, res){
 });
 
 // CREATE - add new campground to DB
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
     //get data from form and add it to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: desc, author: author};
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err);
@@ -31,7 +35,7 @@ router.post("/", function(req, res){
 });
 
 // NEW - show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
     res.render("campgrounds/new.ejs");
 });
 
@@ -47,5 +51,13 @@ router.get("/:id", function(req, res){
         }
     });
 });
+
+// middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
