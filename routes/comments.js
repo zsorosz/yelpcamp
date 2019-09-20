ee0467de00cab49dec4feb4ab523db85245e232d
Zsofia <oroszi.zsofi@gmail.com>
2019-09-20 13:10:40 +0200

    Add authorization to edit and delete comments

--- routes/comments.js
+++ routes/comments.js
@@ -40,7 +40,7 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 // Edit comment
-router.get("/:comment_id/edit", function(req, res){
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
     Comment.findById(req.params.comment_id, function(err, foundComment){
         if(err){
             res.redirect("back");
@@ -51,7 +51,7 @@ router.get("/:comment_id/edit", function(req, res){
 });
 
 //Update comment
-router.put("/:comment_id", function(req, res){
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
         if(err){
             res.redirect("back");
@@ -62,7 +62,7 @@ router.put("/:comment_id", function(req, res){
 });
 
 //Delete comment
-router.delete("/:comment_id", function(req, res){
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
     //findById and remove
     Comment.findByIdAndRemove(req.params.comment_id, function(err){
         if(err){
@@ -78,6 +78,25 @@ function isLoggedIn(req, res, next){
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back");
+            } else {
+                // does the user own the campground
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
