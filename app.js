4701f9893aa51f34d10c09b9ebf3eda3dc02fcf4
Zsofia <oroszi.zsofi@gmail.com>
2019-09-24 23:10:38 +0200

    Add Mongo Atlas

--- app.js
+++ app.js
@@ -19,7 +19,15 @@ var express         = require("express"),
     indexRoutes         = require("./routes/index")
 
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://dbAdmin:ElsoUser001@cluster0-pfkoi.mongodb.net/test?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useCreateIndex: true
+}).then(() => {
+  console.log('Connected to DB');
+}).catch(err => {
+  console.log('ERROR: ', err.message);
+});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
