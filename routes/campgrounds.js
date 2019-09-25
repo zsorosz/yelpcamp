938b633f3ae7d3155e84915f1130596ede39b1e9
Zsofia <oroszi.zsofi@gmail.com>
2019-09-25 14:25:18 +0200

    Fix bug in price feature

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -47,6 +47,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
+    var price = req.body.price;
     var author = {
         id: req.user._id,
         username: req.user.username
@@ -60,7 +61,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
       var lat = data[0].latitude;
       var lng = data[0].longitude;
       var location = data[0].formattedAddress;
-      var newCampground = {name: name, image: image, description: desc, author:author, location: location, lat: lat, lng: lng};
+      var newCampground = {name: name, image: image, description: desc, author:author, price:price, location: location, lat: lat, lng: lng};
       // Create a new campground and save to DB
       Campground.create(newCampground, function(err, newlyCreated){
           if(err){
