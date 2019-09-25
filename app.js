1646e41d72dd1fb37ed59a831927428270be1781
Zsofia <oroszi.zsofi@gmail.com>
2019-09-25 13:21:45 +0200

    Clean up app.js

--- app.js
+++ app.js
@@ -18,16 +18,8 @@ var express         = require("express"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index")
 
-mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
-// mongoose.connect("mongodb+srv://dbAdmin:AdminUser2094@cluster0-pfkoi.mongodb.net/test?retryWrites=true&w=majority", {
-//   useNewUrlParser: true,
-//   useCreateIndex: true
-// }).then(() => {
-//   console.log('Connected to DB');
-// }).catch(err => {
-//   console.log('ERROR: ', err.message);
-// });
-
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
+mongoose.connect(url, {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
