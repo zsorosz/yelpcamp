b3f31c51fd3f4f257d0fa63267d1ece84528ac42
Zsofia <oroszi.zsofi@gmail.com>
2019-09-25 13:09:42 +0200

    Update dburl

--- app.js
+++ app.js
@@ -18,16 +18,17 @@ var express         = require("express"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index")
 
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+// mongoose.connect("mongodb+srv://dbAdmin:AdminUser2094@cluster0-pfkoi.mongodb.net/test?retryWrites=true&w=majority", {
+//   useNewUrlParser: true,
+//   useCreateIndex: true
+// }).then(() => {
+//   console.log('Connected to DB');
+// }).catch(err => {
+//   console.log('ERROR: ', err.message);
+// });
 
-//mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
-mongoose.connect("mongodb+srv://dbAdmin:AdminUser2094@cluster0-pfkoi.mongodb.net/test?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useCreateIndex: true
-}).then(() => {
-  console.log('Connected to DB');
-}).catch(err => {
-  console.log('ERROR: ', err.message);
-});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
